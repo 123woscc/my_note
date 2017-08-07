93189a85a5787cc3948c7c2ae87b684cfa0aebd5
123woscc <1284511710@qq.com>
2017-08-07 10:41:39 +0800

    edit some error

--- auto_get_list.py
+++ auto_get_list.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 
 posts=list()
 title='#Note List'
@@ -7,10 +8,11 @@ git_url='https://github.com/123woscc/my_note/blob/master/'
 
 for index,file in enumerate(os.listdir('.')):
     if file.endswith('.md'):
-        post='{0}.  [{1}]({2}{1})'.format(index,file[:-3],git_url)
+        post='{0}.  [{1}]({2}{3})'.format(index,file[:-3],git_url,urllib.parse.quote(file))
         posts.append(post)
 
 body='\n'.join(posts)
+
 
 with open('README.md','w') as f:
     f.write(body)
