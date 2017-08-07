deacffa0f25093c0ae2b2a698b9b85ae9349f625
123woscc <1284511710@qq.com>
2017-08-07 10:44:24 +0800

    sort list

--- auto_get_list.py
+++ auto_get_list.py
@@ -6,7 +6,7 @@ title='#Note List'
 posts.append(title)
 git_url='https://github.com/123woscc/my_note/blob/master/'
 
-for index,file in enumerate(os.listdir('.')):
+for index,file in enumerate(os.listdir('.').sort()):
     if file.endswith('.md'):
         post='{0}.  [{1}]({2}{3})'.format(index,file[:-3],git_url,urllib.parse.quote(file))
         posts.append(post)
